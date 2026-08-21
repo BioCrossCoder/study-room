9ed96d956c17216ec6c6b51b54cab3877f4482d1
biocrosscoder <viscaria_dignics@163.com>
2026-08-21 15:32:22 +0800

    refactor: extract global styles to ui package

--- web/apps/admin/src/app/layout.tsx
+++ web/apps/admin/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "ui/src/assets/style.css";
 import { ThemeProvider } from "@/contexts/ThemeProvider";
 
 export const metadata: Metadata = {

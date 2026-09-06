ca491156785c197c4a538939ed907de7d05d473a
biocrosscoder <viscaria_dignics@163.com>
2026-09-06 22:27:03 +0800

    feat: define storage structure for business objects in admin

--- web/apps/admin/src/models/orm.ts
+++ web/apps/admin/src/models/orm.ts
@@ -1,6 +1,7 @@
 import {
   integer,
   PgColumn,
+  pgEnum,
   pgTable,
   text,
   uuid,
@@ -17,6 +18,87 @@ function refId(col: PgColumn) {
   return uuid().references(() => col);
 }
 
-const enum TableName {}
+const enum TableName {
+  Resource = "resource",
+  Library = "library",
+  Bookmark = "bookmark",
+  Annotation = "annotation",
+  Summary = "summary",
+}
 
-export const relations = defineRelations({}, (r) => ({}));
+export const library = pgTable(TableName.Library, {
+  id,
+  name,
+  url,
+  description,
+});
+
+export const bookmark = pgTable(TableName.Bookmark, {
+  id,
+  url,
+  xpath: text().notNull(),
+  offset: integer().notNull(),
+});
+
+export const annotationTypeEnum = pgEnum("annotationType", [
+  "replace",
+  "append",
+  "remove",
+  "explain",
+]);
+
+export const annotation = pgTable(TableName.Annotation, {
+  id,
+  url,
+  start: text().notNull(),
+  startOffset: integer().notNull(),
+  end: text().notNull(),
+  endOffset: integer().notNull(),
+  content: text().notNull(),
+  type: annotationTypeEnum("type"),
+});
+
+export const summary = pgTable(TableName.Summary, {
+  id,
+  url,
+  content: text().notNull(),
+});
+
+export const resource = pgTable(TableName.Resource, {
+  id,
+  name,
+  url,
+  description,
+  libraryId: refId(library.id),
+  bookmarkId: refId(bookmark.id),
+});
+
+export const relations = defineRelations(
+  { library, bookmark, resource },
+  (r) => ({
+    resource: {
+      library: r.one.library({
+        from: r.resource.libraryId,
+        to: r.library.id,
+        optional: true,
+      }),
+      bookmark: r.one.bookmark({
+        from: r.resource.bookmarkId,
+        to: r.bookmark.id,
+        optional: true,
+      }),
+    },
+    library: {
+      resources: r.many.resource({
+        from: r.library.id,
+        to: r.resource.libraryId,
+      }),
+    },
+    bookmark: {
+      resources: r.many.resource({
+        from: r.bookmark.id,
+        to: r.resource.bookmarkId,
+      }),
+    },
+  }),
+);

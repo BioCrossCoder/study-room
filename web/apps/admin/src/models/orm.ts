4cb7711004dad115a2ffeaccf5bc44d1c32e0290
biocrosscoder <viscaria_dignics@163.com>
2026-09-13 13:22:19 +0800

    feat: add time fields to orm objects

--- web/apps/admin/src/models/orm.ts
+++ web/apps/admin/src/models/orm.ts
@@ -3,7 +3,9 @@ import {
   PgColumn,
   pgEnum,
   pgTable,
+  PgTimestampConfig,
   text,
+  timestamp,
   uuid,
   varchar,
 } from "drizzle-orm/pg-core";
@@ -13,6 +15,10 @@ const id = uuid("id").primaryKey();
 const name = varchar({ length: 100 }).notNull().unique();
 const url = text().notNull().unique();
 const description = text().notNull();
+
+function timestamptz(config?: PgTimestampConfig) {
+  return timestamp({ ...config, withTimezone: true });
+}
 
 function refId(col: PgColumn) {
   return uuid().references(() => col);
@@ -31,6 +37,8 @@ export const library = pgTable(TableName.Library, {
   name,
   url,
   description,
+  createAt: timestamptz(),
+  updateAt: timestamptz(),
 });
 
 export const bookmark = pgTable(TableName.Bookmark, {
@@ -38,6 +46,8 @@ export const bookmark = pgTable(TableName.Bookmark, {
   url,
   xpath: text().notNull(),
   offset: integer().notNull(),
+  createAt: timestamptz(),
+  updateAt: timestamptz(),
 });
 
 export const annotationTypeEnum = pgEnum("annotationType", [
@@ -56,12 +66,16 @@ export const annotation = pgTable(TableName.Annotation, {
   endOffset: integer().notNull(),
   content: text().notNull(),
   type: annotationTypeEnum("type"),
+  createAt: timestamptz(),
+  updateAt: timestamptz(),
 });
 
 export const summary = pgTable(TableName.Summary, {
   id,
   url,
   content: text().notNull(),
+  createAt: timestamptz(),
+  updateAt: timestamptz(),
 });
 
 export const resource = pgTable(TableName.Resource, {
@@ -71,6 +85,9 @@ export const resource = pgTable(TableName.Resource, {
   description,
   libraryId: refId(library.id),
   bookmarkId: refId(bookmark.id),
+  createAt: timestamptz(),
+  updateAt: timestamptz(),
+  lastVisit: timestamptz(),
 });
 
 export const relations = defineRelations(

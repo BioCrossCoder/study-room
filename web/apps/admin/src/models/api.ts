913192f013bb2936db5bf23091f95f71f2e620ce
biocrosscoder <viscaria_dignics@163.com>
2026-09-20 23:49:54 +0800

    feat: add api verification schemas for reading operations

--- web/apps/admin/src/models/api.ts
+++ web/apps/admin/src/models/api.ts
@@ -5,11 +5,32 @@ const id = z.uuid();
 const name = z.string().min(1).max(100);
 const url = z.url();
 const description = z.string().nonempty();
+const time = z.object({
+  gt: z.date().optional(),
+  lt: z.date().optional(),
+  eq: z.date().optional(),
+  ne: z.date().optional(),
+});
+const sort = z.array(
+  z.object({
+    field: z.enum(["createAt", "updateAt", "id"]),
+    direction: z.enum(["asc", "desc"]),
+  }),
+);
 
 export const Library = {
   create: z.object({ name, url, description }),
   update: z.object({ id, name, description }),
   delete: z.object({ id }),
+  get: z.union([z.object({ id }), z.object({ name }), z.object({ url })]),
+  list: z.object({
+    filter: z.object({
+      name: name.optional(),
+      createAt: time.optional(),
+      updateAt: time.optional(),
+    }),
+    sort,
+  }),
 };
 
 const xpath = z.string().nonempty();
@@ -18,6 +39,14 @@ export const Bookmark = {
   create: z.object({ url, xpath, offset }),
   update: z.object({ id, xpath, offset }),
   delete: z.object({ id }),
+  get: z.union([z.object({ id }), z.object({ url })]),
+  list: z.object({
+    filter: z.object({
+      createAt: time.optional(),
+      updateAt: time.optional(),
+    }),
+    sort,
+  }),
 };
 
 const start = z.string().nonempty();
@@ -40,12 +69,30 @@ export const Annotation = {
     type: annotationType,
   }),
   delete: z.object({ id }),
+  get: z.object({ id }),
+  list: z.object({
+    filter: z.object({
+      url: url.optional(),
+      type: annotationType.optional(),
+      createAt: time.optional(),
+      updateAt: time.optional(),
+    }),
+    sort,
+  }),
 };
 
 export const Summary = {
   create: z.object({ url, content }),
   update: z.object({ id, content }),
   delete: z.object({ id }),
+  get: z.union([z.object({ id }), z.object({ url })]),
+  list: z.object({
+    filter: z.object({
+      createAt: time.optional(),
+      updateAt: time.optional(),
+    }),
+    sort,
+  }),
 };
 
 const nullableId = z.uuid().nullable();
@@ -65,4 +112,20 @@ export const Resource = {
     bookmarkId: nullableId,
   }),
   delete: z.object({ id }),
+  get: z.union([z.object({ id }), z.object({ name }), z.object({ url })]),
+  list: z.object({
+    filter: z.object({
+      name: name.optional(),
+      libraryId: id.optional(),
+      createAt: time.optional(),
+      updateAt: time.optional(),
+      lastVisit: time.optional(),
+    }),
+    sort: z.array(
+      z.object({
+        field: z.enum(["createAt", "updateAt", "id", "lastVisit"]),
+        direction: z.enum(["asc", "desc"]),
+      }),
+    ),
+  }),
 };
